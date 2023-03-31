97fdf41c7d425e958a94ea7a9731893f5ccf3524
onury5506 <onuryildizsai@gmail.com>
2023-03-31 09:41:50 +0300

    fix: guestToken optimization

--- twitter/guestToken.js
+++ twitter/guestToken.js
@@ -2,7 +2,15 @@ import variables from "./variables.js"
 
 const url = "https://api.twitter.com/1.1/guest/activate.json"
 
+let lastGuestToken = ""
+let guestTokenCounter = 0
+
 export default async function guestToken(){
+    if(guestTokenCounter > 0){
+        guestTokenCounter--
+        return lastGuestToken
+    }
+    guestTokenCounter = 5
     let res = await fetch(url,{
         method:"post",
         headers:{
